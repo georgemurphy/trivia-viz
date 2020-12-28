f83a86b5e74dc538a62c31c835f536ea9bd01780
georgemurphy <georgelmurphy@gmail.com>
2020-12-28 18:34:29 -0500

    wrapper func to wait for DOM load

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-Promise.all([
-	d3.csv('data/data.csv')
-]).then(function(files){
-	console.log(files[0]);
+// wait until everything on page loads
+document.addEventListener("DOMContentLoaded", function() {
+	
+	// read data files
+	Promise.all([
+		d3.csv('data/data.csv')
+	]).then(function(files){
+		console.log(files[0]);
+	});
+
 });
